f9b639bcddf68f7fdda0e2f919c96b4084ec9691
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2023-06-06 20:36:21 +0700

    Navbar update

--- resources/views/Partials/navbar.blade.php
+++ resources/views/Partials/navbar.blade.php
@@ -4,12 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <link href="{{asset('css/navbar.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('css/semua.css')}}" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        
 
         <title>Menu</title>
         </head>
@@ -17,11 +18,13 @@
                 @if( auth()->check() )
                 <nav class="nav browser-default">
                         <div class="nav-wrapper browser-default">
-                                <a href="{{route('dashboard')}}" class="brand-logo">Mobil<span class='sred'>Malang</a>
+                                <a href="{{route('dashboard')}}" class="brand-logo">Car<span class='sred'>Rent</a>
+                                <a href="{{route('profView')}}"class="ungu" style="padding-left: 220px">Profil</a>
+                                <a href="{{route('userCreateHist')}}" class="ungu" style="padding-left: 40px">Riwayat</a>
                                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons menu">menu</i></a>
                                 <ul class="right hide-on-med-and-down other_nav">
-                                <li><a href="{{route('profView')}}" class="ungu">Profil</a></li>
-                                <li><a href="{{route('userCreateHist')}}" class="ungu">Riwayat</a></li>
+                                {{-- <li><a href="{{route('profView')}}" class="ungu">Profil</a></li> --}}
+                                {{-- <li><a href="{{route('userCreateHist')}}" class="ungu">Riwayat</a></li> --}}
                                 <li><a href="{{route('sessDestroy')}}" class="ungu keluar">Keluar</a></li>
                                 </ul>
                                 <ul class="side-nav" id="mobile-demo">
